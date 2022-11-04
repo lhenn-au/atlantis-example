490f50091b82d140bbd5737b15a8eb16956fe4d0
lahenn <lhenn@nuvalence.io>
2022-11-04 11:52:00 -0400

    add resource e to terraform-a

--- terraform-a/test.tf
+++ terraform-a/test.tf
@@ -2,4 +2,6 @@ resource "null_resource" "example-a" {}
 resource "null_resource" "example-b" {}
 resource "null_resource" "example-c" {}
 resource "null_resource" "example-d" {}
+resource "null_resource" "example-e" {}
+
 
