efc85883bc76cffb61e929b5645273c717c6a20d
lahenn <lhenn@nuvalence.io>
2022-11-16 16:38:04 -0500

    clean up policy config

--- pull-policies.sh
+++ pull-policies.sh
@@ -1,13 +1,19 @@
-echo 'hello world'
+#!/usr/bin/env bash
+
+set -o errexit
+
+echo 'Retrieving policies'
+
 POLICY_DIR="/Users/laurahenn/atlantis-policy-test/policies"
 REMOTE="git@github.com:lhenn-au/atlantis-example.git"
+
 if [ -d "$POLICY_DIR" ]; then
-  echo "$POLICY_DIR does exist."
+  echo "pulling master branch of $POLICY_DIR"
   cd $POLICY_DIR
   git switch master
   git pull
 fi
 if [ ! -d "$POLICY_DIR" ]; then
-  echo "$POLICY_DIR does not exist."
+  echo "$POLICY_DIR not found in expected location, cloning.."
   git clone "$REMOTE" "/Users/laurahenn/atlantis-policy-test/"
 fi
